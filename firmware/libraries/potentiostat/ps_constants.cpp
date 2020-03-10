6d950a3cf6a48841c3b44fe5357c4c1a5e818428
Selva Nair <selva.nair@gmail.com>
2020-03-09 23:01:15 -0400

    Bump up the version!
    Signed-off-by: Selva Nair <selva.nair@gmail.com>

--- firmware/libraries/potentiostat/ps_constants.cpp
+++ firmware/libraries/potentiostat/ps_constants.cpp
@@ -4,7 +4,7 @@
 
 namespace ps
 { 
-    const String FirmwareVersion = String("0.0.6");
+    const String FirmwareVersion = String("0.0.7");
 
     // Serial parameters
     const uint32_t UsbSerialBaudrate = 115200;
